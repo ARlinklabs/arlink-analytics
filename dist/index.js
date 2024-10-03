03d52c9555cfa7de819c16f1b3f61d60e101e09d
internet_trash <naiknischal92@gmail.com>
2024-10-04 01:16:31 +0530

    add readme

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "AnalyticsWrapper", {
+  enumerable: true,
+  get: function get() {
+    return _AnalyticsWrapper["default"];
+  }
+});
+var _AnalyticsWrapper = _interopRequireDefault(require("./AnalyticsWrapper"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
